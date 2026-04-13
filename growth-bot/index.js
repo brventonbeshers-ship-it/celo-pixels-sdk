f16d57bf6b6a9637f582e5f4c802575530265f47
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-13 03:19:54 +0300

    ci: add growth pulse workflow for SDK downloads

---
+++ growth-bot/index.js
@@ -0,0 +1,5 @@
+import * as sdk from 'celo-pixels-sdk';
+
+console.log('Growth bot running for celo-pixels-sdk...');
+console.log('SDK exports:', Object.keys(sdk).join(', '));
+console.log('Smoke check passed.');
